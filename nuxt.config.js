65758b5a8ec6d3df1afdc4296a99aacea4ba5380
mar-sh <marasi.af@gmail.com>
2022-10-31 16:14:40 +0700

    add typography styles, add home page

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,11 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/reset.css'],
+  css: [
+    '@/assets/reset.css',
+    '@/assets/style.scss',
+    '@/assets/typography.scss',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    Home Page
-  </div>
+  <main class="page">
+    <div class="hero">
+      <h1>The Best Catalog Ever</h1>
+      <h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+        adipisci et excepturi necessitatibus cumque voluptatum, sunt possimus,
+        saepe numquam quod facere dignissimos.
+      </h3>
+
+      <button><nuxt-link to="/categories">Browse Now</nuxt-link></button>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -9,3 +18,42 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style lang="scss">
+main {
+  .hero {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 2rem;
+
+    h1 {
+      margin-bottom: 1rem;
+    }
+
+    h3 {
+      margin-bottom: 1rem;
+    }
+  }
+}
+
+@media only screen and (min-width: 768px) {
+  main {
+    .hero {
+      padding-top: 5rem;
+      padding-bottom: 5rem;
+      padding-left: 5rem;
+      padding-right: 5rem;
+    }
+
+    h1 {
+      margin-bottom: 2rem;
+    }
+
+    h3 {
+      margin-bottom: 2rem;
+    }
+  }
+}
+</style>
