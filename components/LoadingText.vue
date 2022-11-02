609470d06b52f41ea53071ab5844e70485a6ee5f
mar-sh <marasi.af@gmail.com>
2022-11-02 09:38:38 +0700

    add loading text component

---
+++ components/LoadingText.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-if="isLoading">
+    <p class="loader-text">{{ `${text}...` }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadingText',
+
+  props: {
+    isLoading: Boolean,
+    text: {
+      type: String,
+      default: 'Loading',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.loader-text {
+  text-align: center;
+  font-size: 24px;
+  font-weight: 600;
+}
+</style>
